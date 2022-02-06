22dd646dc6a196f5b15cfdcd1e86fd30960e0402
eldzh123 <eldzh123@gmail.com>
2022-02-06 16:43:51 +0900

    Solved 백준 1000
    A+B

---
+++ 백준/1000/1000.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+let input = fs.readFileSync(path.join(__dirname, "input.txt")).toString();
+input = input.split(' ');
+const A = +input[0];
+const B = +input[1];
+//    A : 1
+//    B : 2
+
+function solution(A, B)
+{
+    console.log(A + B);
+}
+
+solution(A, B);
