8b466442bdeebf3c91c584259e7ff96112307f1e
eldzh123 <eldzh123@gmail.com>
2022-02-07 05:46:19 +0900

    Solved 백준 10950
    A+B - 3

---
+++ 백준/10950/10950.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+let input = fs.readFileSync(path.join(__dirname, "input.txt")).toString();
+input = input.split("\n");
+const C = +input[0];
+const testCase = [];
+
+for (let i = 1; i < input.length; i++) {
+  const arr = input[i].split(" ");
+  const newArr = [];
+  for (let j = 0; j < arr.length; j++) {
+    newArr.push(+arr[j]);
+  }
+  testCase.push(newArr);
+}
+//  C : 5
+//  testCase : [[1, 1] ... [5, 2]]
+
+function solution(C, testCase) {
+  for (let i = 0; i < C; i++) {
+    const sum = testCase[i][0] + testCase[i][1];
+    console.log(sum);
+  }
+}
+
+solution(C, testCase);
