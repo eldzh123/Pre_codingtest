2db9a2cc58c540dab3d183449362183f4e992b03
eldzh123 <eldzh123@gmail.com>
2022-02-06 17:36:37 +0900

    Solved 백준 10869
    사칙연산

---
+++ 백준/10869/10869.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+let input = fs.readFileSync(path.join(__dirname, "input.txt")).toString();
+input = input.split(' ');
+const A = +input[0];
+const B = +input[1];
+//    A : 7
+//    B : 3
+
+function solution(A, B)
+{
+    console.log(A + B);
+    console.log(A - B);
+    console.log(A * B);
+    console.log(Math.floor(A / B));
+    console.log(A % B);
+}
+
+solution(A, B);
