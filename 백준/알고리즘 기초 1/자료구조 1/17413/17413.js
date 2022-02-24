6e4680577973f9d39ec55f43c1056b40205fb122
eldzh123 <eldzh123@gmail.com>
2022-02-24 20:10:01 +0900

    Solved 백준 17413
    단어 뒤집기 2

---
+++ 백준/알고리즘 기초 1/자료구조 1/17413/17413.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const path = require("path");
+const input = fs
+  .readFileSync(path.join(__dirname, "input.txt"))
+  .toString()
+  .trim();
+console.log(input);
+
+function revWord(word) {
+  const arr = word.split("");
+  let result = "";
+  for (let i = arr.length - 1; i >= 0; i--) {
+    result += arr[i];
+  }
+  return result;
+}
+
+function solution(input) {
+  const arr = input.split("");
+  let result = "";
+  let word = "";
+  let isTag = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "<") {
+      isTag = true;
+      result += revWord(word);
+      word = "";
+      result += arr[i];
+    } else if (arr[i] === ">") {
+      isTag = false;
+      result += arr[i];
+    } else if (arr[i] === " ") {
+      if (isTag) {
+        result += arr[i];
+      } else {
+        result += `${revWord(word)} `;
+        word = "";
+      }
+    } else if (i === arr.length - 1) {
+      word += arr[i];
+      result += revWord(word);
+    } else {
+      if (isTag) {
+        result += arr[i];
+      } else {
+        word += arr[i];
+      }
+    }
+  }
+  console.log(result);
+}
+
+solution(input);
